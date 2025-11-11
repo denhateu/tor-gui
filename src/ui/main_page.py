c8cd4408672589ea372218b7cebf1c43d64d5122
denhateu <danilaksenow101@gmail.com>
2025-11-11 18:49:11 +0500

    feat: added some containers and start button

--- src/ui/main_page.py
+++ src/ui/main_page.py
@@ -1,6 +1,32 @@
 import tkinter as tk
+from tkinter import ttk
 
 
 class MainPage(tk.Frame):
     def __init__(self, parent, controller):
         tk.Frame.__init__(self, parent)
+
+        self.create_widgets()
+
+    def create_widgets(self):
+        # Main container
+        main_container = tk.Frame(self)
+        main_container.pack(
+          anchor="center",
+          fill="both",
+          expand=True
+        )
+
+        # Content container
+        content_container = tk.Frame(main_container)
+        content_container.pack(
+          anchor="center",
+          fill="both",
+          expand=True
+        )
+
+        start_button = ttk.Button(
+          content_container,
+          text="Запустить"
+        )
+        start_button.pack()
