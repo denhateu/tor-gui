55b0942c9b427fecd1025cde9b6f866d0dba424b
denhateu <danilaksenow101@gmail.com>
2026-01-18 21:25:56 +0500

    chore: removed use null.css and use style.css with tailwind

--- src/renderer/index.js
+++ src/renderer/index.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
-import "../assets/css/null.css";
+import "../assets/css/style.css";
 import App from "./App";
 
 const root = createRoot(document.getElementById("root"));
